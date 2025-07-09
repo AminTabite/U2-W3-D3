7f2ddce2624e300ce5598ae5f2f35b6be23c4971
AminTabite <Aminepicode13@gmail.com>
2025-07-09 16:16:08 +0200

    .
    manca solo il pulsante per adesso, cerco di risolvere

--- libreria.js
+++ libreria.js
@@ -1,9 +1,34 @@
 const catalogoLibri = function () {
-    fetch('https,//striveschool-api.herokuapp.com/books')
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((response) => {
+      console.log("RESPONSE", response);
+      if (response.ok) {
+        // la risposta dal server è ok, estrapoliamo il JSON
+        return response.json();
+      } else {
+        // la Response ha un problema!
+        throw new Error("La response da Open Meteo non è ok");
+      }
+    })
 
+    .then((datalibri) => {
+      console.log(datalibri);
 
+      const card = document.getElementById("book");
 
+      datalibri.forEach((libro) => {
+        card.innerHTML += `
+             <div class= col col-12 col-md-6 col-lg-4 m-5 g-3 p-3>  <div class="card bg-dark text-primary m-4 ">
+  <img class=w-100   src="${libro.img}" class="card-img-top" id="img" alt="copertina">
+  <div class="card-body">
+    <h5 class="card-title" id="title">${libro.title}</h5>
+    <p class="card-text" id="price">${libro.price}</p>
+    <a href="#" class="btn btn-primary">Delete</a>
+  </div>
+</div> </div>
+        `;
+      });
+    });
+};
 
-
-
-}
+catalogoLibri();
